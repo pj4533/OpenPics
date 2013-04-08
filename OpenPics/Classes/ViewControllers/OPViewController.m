d8fdb85f310636cd395b4e285adbbca3b595f5cb
David Ohayon <ohayon.1@gmail.com>
2013-04-08 15:26:26 -0400

    adds clear textfield button

--- OpenPics/Classes/ViewControllers/OPViewController.m
+++ OpenPics/Classes/ViewControllers/OPViewController.m
@@ -131,6 +131,7 @@
 - (UICollectionReusableView*) collectionView:(UICollectionView *)collectionView viewForSupplementaryElementOfKind:(NSString *)kind atIndexPath:(NSIndexPath *)indexPath {
     
     OPHeaderReusableView *header = [collectionView dequeueReusableSupplementaryViewOfKind:kind withReuseIdentifier:@"header" forIndexPath:indexPath];
+    header.internalTextField.clearButtonMode = UITextFieldViewModeWhileEditing;
     header.delegate = self;
     [header.providerButton setTitle:_currentProvider.providerName forState:UIControlStateNormal];
 
