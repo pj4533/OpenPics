7ccb23c03ab1d9acdd5ef8891b3b0916bf0b38da
PJ Gray <pj@evertrue.com>
2013-04-20 14:16:22 -0400

    Europeana tweakage

--- OpenPics/Classes/Providers/Europeana/OPEuropeanaProvider.m
+++ OpenPics/Classes/Providers/Europeana/OPEuropeanaProvider.m
@@ -58,7 +58,9 @@ NSString * const OPProviderTypeEuropeana = @"com.saygoodnight.europeana";
                 NSDictionary* opImageDict = @{
                                               @"imageUrl": imageUrl,
                                               @"title" : titleString,
-                                              @"providerSpecific" : @{@"europeanaItem":itemDict[@"link"]}
+                                              @"providerSpecific" : @{
+                                                        @"europeanaItem":itemDict[@"link"]
+                                                      }
                                               };
                 OPImageItem* item = [[OPImageItem alloc] initWithDictionary:opImageDict];
                 [retArray addObject:item];
@@ -82,25 +84,108 @@ NSString * const OPProviderTypeEuropeana = @"com.saygoodnight.europeana";
 }
 
 - (void) upRezItem:(OPImageItem *) item withCompletion:(void (^)(NSURL *uprezImageUrl))completion {
-//    NSDictionary* providerSpecific = item.providerSpecific;
-//    
-//    NSString* europeanaItem = providerSpecific[@"europeanaItem"];
-//    NSLog(@"%@", europeanaItem);
-//    NSURL* url = [NSURL URLWithString:europeanaItem];
-//    NSURLRequest *request = [NSURLRequest requestWithURL:url];
-//    AFJSONRequestOperation *operation = [AFJSONRequestOperation JSONRequestOperationWithRequest:request success:^(NSURLRequest *request, NSHTTPURLResponse *response, id JSON) {
-//        
-//        NSLog(@"%@", JSON);
-//        NSDictionary* imageInfo = JSON[@"imageinfo"];
-//        if (imageInfo) {
-//            if (completion) {
-//                completion([NSURL URLWithString:@""]);
-//            }
-//        }
-//    } failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error, id JSON) {
-//        NSLog(@"image info error: %@", error);
-//    }];
-//    [operation start];
+    NSDictionary* providerSpecific = item.providerSpecific;
+    NSString* europeanaItem = providerSpecific[@"europeanaItem"];
+    NSURL* url = [NSURL URLWithString:europeanaItem];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    AFJSONRequestOperation *operation = [AFJSONRequestOperation JSONRequestOperationWithRequest:request success:^(NSURLRequest *request, NSHTTPURLResponse *response, id JSON) {
+        
+        NSString* urlString = item.imageUrl.absoluteString;
+        NSDictionary* objectDict = JSON[@"object"];
+        if (objectDict) {
+            NSArray* aggregationsArray = objectDict[@"aggregations"];
+            if (aggregationsArray && aggregationsArray.count) {
+                NSDictionary* aggregationDict = aggregationsArray[0];
+                NSDictionary* edmDataProviderDict = aggregationDict[@"edmDataProvider"];
+
+                
+                // Using edm DATA provider
+                if (edmDataProviderDict) {
+                    NSArray* defArray = edmDataProviderDict[@"def"];
+                    if (defArray && defArray.count) {
+                        NSString* dataProviderString = defArray[0];
+                        
+                        //
+                        if ([dataProviderString isEqualToString:@"Kirklees Image Archive OAI Feed"]) {
+                            NSLog(@"KNOWN: %@", dataProviderString);
+                            NSArray* proxiesArray = objectDict[@"proxies"];
+                            if (proxiesArray && proxiesArray.count) {
+                                NSDictionary* proxyDict = proxiesArray[0];
+                                NSDictionary* identifierDict = proxyDict[@"dcIdentifier"];
+                                NSArray* defArray = identifierDict[@"def"];
+                                if (defArray && defArray.count) {
+                                    NSString* dcIdentifier = defArray[0];
+                                    NSArray* semiComponents = [dcIdentifier componentsSeparatedByString:@";"];
+                                    NSArray* ampComponents = [semiComponents.lastObject componentsSeparatedByString:@"&"];
+                                    NSString* itemId = ampComponents[0];
+                                    NSDictionary* identifierDict = proxyDict[@"dcSubject"];
+                                    NSArray* defArray = identifierDict[@"def"];
+                                    if (defArray && defArray.count) {
+                                        NSString* dcSubject = defArray[0];
+                                        if ([dcSubject isEqualToString:@"Kirklees"]) {
+                                            urlString = [NSString stringWithFormat:@"https://www.hpacde.org.uk/kirklees/jpgh_kirklees/%@.jpg", itemId];
+                                        } else if ([dcSubject isEqualToString:@"Bamforth"]) {
+                                            urlString = [NSString stringWithFormat:@"https://www.hpacde.org.uk/kirklees/jpgh_bamforth/%@.jpg", itemId];                                            
+                                        } else {
+                                            NSLog(@"UNKNOWN SUBJECT: %@", dcSubject);
+                                        }
+                                    }
+                                }
+                            }
+                        } else if ([dataProviderString isEqualToString:@"National Library of Wales"]) {
+                            NSLog(@"KNOWN: %@", dataProviderString);
+                            NSString* edmObject = aggregationDict[@"edmObject"];
+                            if (edmObject) {
+                                urlString = [edmObject stringByReplacingOccurrencesOfString:@"-13" withString:@"-11"];
+                            } else {
+                                NSLog(@"%@", objectDict);
+                            }
+                        
+                        } else if ([dataProviderString isEqualToString:@"Leodis - A photographic archive of Leeds"]) {
+                            NSLog(@"KNOWN: %@", dataProviderString);
+                            NSArray* proxiesArray = objectDict[@"proxies"];
+                            if (proxiesArray && proxiesArray.count) {
+                                NSDictionary* proxyDict = proxiesArray[0];
+                                NSDictionary* identifierDict = proxyDict[@"dcIdentifier"];
+                                NSArray* defArray = identifierDict[@"def"];
+                                if (defArray && defArray.count) {
+                                    NSString* dcIdentifier = defArray[0];
+                                    NSArray* equalsComponents = [dcIdentifier componentsSeparatedByString:@"="];
+                                    NSString* itemId = equalsComponents[1];
+                                    NSString* collectionId = [itemId substringFromIndex:itemId.length - 2];
+                                    urlString = [NSString stringWithFormat:@"http://www.leodis.net/imagesLeodis/screen/%@/%@.jpg", collectionId, itemId];
+                                }
+                            }
+                            
+                        } else {
+                            NSLog(@"UNKNOWN: %@", dataProviderString);
+//                            NSLog(@"%@", objectDict);
+                        }
+                    }
+                } else {
+                    NSDictionary* edmProviderDict = aggregationDict[@"edmProvider"];
+                    if (edmProviderDict) {
+                        NSArray* defArray = edmProviderDict[@"def"];
+                        if (defArray && defArray.count) {
+                            NSString* providerString = defArray[0];
+                            
+                            NSLog(@"UNKNOWN PROVIDER: %@", providerString);
+                        }
+                    }
+                }
+            }
+        }
+        
+        if (![urlString isEqualToString:item.imageUrl.absoluteString]) {
+            if (completion) {
+                completion([NSURL URLWithString:urlString]);
+            }
+        }
+
+    } failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error, id JSON) {
+        NSLog(@"image info error: %@", error);
+    }];
+    [operation start];
 }
 
 @end
