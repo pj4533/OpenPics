9ce90b76a4b4f9740bc8a518c30c2f5b0fca30ab
PJ Gray <pj@evertrue.com>
2013-05-11 15:31:55 -0400

    Added code for reporting to StatHat

--- OpenPics/OPAppDelegate.m
+++ OpenPics/OPAppDelegate.m
@@ -22,6 +22,13 @@
 
 #import "OPAppearance.h"
 #import <Crashlytics/Crashlytics.h>
+#import "AFStatHatClient.h"
+
+@interface OPAppDelegate () {
+    NSDate* _appBecameActiveDate;
+}
+
+@end
 
 @implementation OPAppDelegate
 
@@ -54,29 +61,38 @@
 
 - (void)applicationWillResignActive:(UIApplication *)application
 {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+#if !TARGET_IPHONE_SIMULATOR
+#ifdef kOPAPPTOKEN_STATHAT
+    AFStatHatClient* stathat = [[AFStatHatClient alloc] initWithEZKey:kOPAPPTOKEN_STATHAT];
+    NSTimeInterval secondsAppWasActive = [[NSDate date] timeIntervalSinceDate:_appBecameActiveDate];
+    [stathat postEZStat:@"Length of time using OpenPics (in minutes)" withValue:@(secondsAppWasActive/60)];
+#endif
+#endif
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+
+    _appBecameActiveDate = [NSDate date];
+    
+#if !TARGET_IPHONE_SIMULATOR
+#ifdef kOPAPPTOKEN_STATHAT
+    AFStatHatClient* stathat = [[AFStatHatClient alloc] initWithEZKey:kOPAPPTOKEN_STATHAT];
+    [stathat postEZStat:@"OpenPics Launches" withCount:@1];
+#endif
+#endif
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
 - (BOOL)application:(UIApplication *)application
