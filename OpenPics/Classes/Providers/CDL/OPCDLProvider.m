2f4e037c34b3237620d0d3df9b0f5f0904aa2ac4
PJ Gray <pj@evertrue.com>
2013-05-12 12:28:48 -0400

    More results and general lovin to Cali provider

--- OpenPics/Classes/Providers/CDL/OPCDLProvider.m
+++ OpenPics/Classes/Providers/CDL/OPCDLProvider.m
@@ -45,7 +45,7 @@ NSString * const OPProviderTypeCDL = @"com.saygoodnight.cdl";
     [[AFCDLClient sharedClient] getPath:@"search" parameters:parameters success:^(AFHTTPRequestOperation *operation, id responseObject) {
         
         AFKissXMLRequestOperation* kissXMLOperation = (AFKissXMLRequestOperation*) operation;
-        
+                
         if ([kissXMLOperation.responseXMLDocument isKindOfClass:[DDXMLDocument class]]) {
             NSError *error;
             
@@ -59,38 +59,43 @@ NSString * const OPProviderTypeCDL = @"com.saygoodnight.cdl";
                     NSString* totalDocs = [[groupElement attributeForName:@"totalDocs"] stringValue];
                     NSString* endDoc = [[groupElement attributeForName:@"endDoc"] stringValue];
                     
-//                    NSLog(@"TOTAL: %@   END: %@", totalDocs, endDoc);
-
+                    NSLog(@"TOTAL: %@   END: %@", totalDocs, endDoc);
+                    
                     NSArray* docHits = [groupElement nodesForXPath:@"docHit" error:&error];
                     for (DDXMLElement* itemElement in docHits) {
+
+                        NSString* path = [[itemElement attributeForName:@"path"] stringValue];
+                        NSArray* dotComponents  = [path componentsSeparatedByString:@"."];
+                        NSArray* colonComponents = [dotComponents[0] componentsSeparatedByString:@":"];
+                        NSString* pathString = colonComponents[1];
+                        NSString* imageId = pathString.lastPathComponent;
+                        NSString* noLastPathComponent = [pathString stringByDeletingLastPathComponent];
+                        NSString* urlString = [NSString stringWithFormat:@"http://imgzoom.cdlib.org/Converter?id=/%@/files/%@-z1.jp2&w=300",noLastPathComponent,imageId];
+
                         DDXMLElement *metaElement = [[itemElement nodesForXPath:@"meta" error:&error] objectAtIndex:0];
-                        NSArray* typeNodes = [metaElement nodesForXPath:@"type" error:&error];
-                        if (typeNodes.count) {
-                            DDXMLElement *typeElement = typeNodes[0];
+                        NSString* titleString = @"";
+                        NSArray* titleNodes = [metaElement nodesForXPath:@"title" error:&error];
+                        if (titleNodes.count) {
+                            titleString = [titleNodes[0] stringValue];
+                        }
+                        
+                        NSMutableDictionary* providerSpecific = [NSMutableDictionary dictionary];
+
+                        NSArray* referenceImages = [metaElement nodesForXPath:@"reference-image" error:&error];
+                        if (referenceImages.count) {
+                            NSString* src = [[referenceImages[0] attributeForName:@"src"] stringValue];
+                            urlString = [NSString stringWithFormat:@"http://content.cdlib.org%@", src];
                             
-                            if ([typeElement.stringValue.lowercaseString isEqualToString:@"image"] || [typeElement.stringValue.lowercaseString isEqualToString:@"image collection"]) {
-                                NSArray* elementArray = [metaElement nodesForXPath:@"reference-image" error:&error];
-                                NSString* imagePath = @"";
-                                if (elementArray.count) {
-                                    
-                                    DDXMLElement *referenceImage = elementArray[0];
-                                    NSString* srcString = [[referenceImage attributeForName:@"src"] stringValue];
-                                    imagePath = [NSString stringWithFormat:@"http://content.cdlib.org%@", srcString];
-                                    
-                                    NSURL* imageUrl = [NSURL URLWithString:imagePath];
-                                    NSString* titleString = @"";
-                                    NSArray* titleNodes = [metaElement nodesForXPath:@"title" error:&error];
-                                    if (titleNodes.count) {
-                                        titleString = [titleNodes[0] stringValue];
-                                    }
-                                    
-                                    NSDictionary* opImageDict = @{@"imageUrl": imageUrl, @"title" : titleString};
-                                    OPImageItem* item = [[OPImageItem alloc] initWithDictionary:opImageDict];
-                                    [retArray addObject:item];
-                                }
-                                
+                            if (referenceImages.count > 1) {
+                                NSString* higherResRefImage = [NSString stringWithFormat:@"http://content.cdlib.org%@", [[referenceImages.lastObject attributeForName:@"src"] stringValue]];
+                                providerSpecific[@"refimage"] = higherResRefImage;
                             }
-                        }                        
+                        }
+                                                
+                        NSURL* imageUrl = [NSURL URLWithString:urlString];
+                        NSDictionary* opImageDict = @{@"imageUrl": imageUrl, @"title" : titleString};
+                        OPImageItem* item = [[OPImageItem alloc] initWithDictionary:opImageDict];
+                        [retArray addObject:item];
                     }
                     if (endDoc.integerValue < totalDocs.integerValue) {
                         returnCanLoadMore = YES;
@@ -106,6 +111,24 @@ NSString * const OPProviderTypeCDL = @"com.saygoodnight.cdl";
     } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
         NSLog(@"ERROR: %@\n%@\n%@", error.localizedDescription,error.localizedFailureReason,error.localizedRecoverySuggestion);
     }];
+}
+
+- (void) upRezItem:(OPImageItem *) item withCompletion:(void (^)(NSURL *uprezImageUrl, OPImageItem* item))completion {
+    
+    NSString* urlString = item.imageUrl.absoluteString;
+    NSDictionary* providerSpecific = item.providerSpecific;
+
+    urlString = [urlString stringByReplacingOccurrencesOfString:@"w=300" withString:@"w=1200"];
+
+    if (providerSpecific[@"refimage"]) {
+        urlString = providerSpecific[@"refimage"];
+    }
+
+    if (![urlString isEqualToString:item.imageUrl.absoluteString]) {
+        if (completion) {
+            completion([NSURL URLWithString:urlString], item);
+        }
+    }
 }
 
 @end
