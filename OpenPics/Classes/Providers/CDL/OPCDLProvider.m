a856190f0c34a19c1eb7f6b383e23e12e8d82d58
PJ Gray <pj@evertrue.com>
2013-05-12 11:33:02 -0400

    Fixes #34 Crash with California

--- OpenPics/Classes/Providers/CDL/OPCDLProvider.m
+++ OpenPics/Classes/Providers/CDL/OPCDLProvider.m
@@ -43,56 +43,60 @@ NSString * const OPProviderTypeCDL = @"com.saygoodnight.cdl";
 //    NSLog(@"(CDL GET) %@", parameters);
     
     [[AFCDLClient sharedClient] getPath:@"search" parameters:parameters success:^(AFHTTPRequestOperation *operation, id responseObject) {
-
+        
         AFKissXMLRequestOperation* kissXMLOperation = (AFKissXMLRequestOperation*) operation;
         
         if ([kissXMLOperation.responseXMLDocument isKindOfClass:[DDXMLDocument class]]) {
             NSError *error;
-            NSArray *xmlItems = [kissXMLOperation.responseXMLDocument nodesForXPath:@"//docHit" error:&error];
             
-            // this must be wrong...
-            NSArray *resultItems = [kissXMLOperation.responseXMLDocument nodesForXPath:@"crossQueryResult" error:&error];
             NSMutableArray* retArray = [NSMutableArray array];
             BOOL returnCanLoadMore = NO;
-            if (resultItems.count) {
-                DDXMLElement *crossQueryResult = resultItems[0];
-                NSString* totalDocs = [[crossQueryResult attributeForName:@"totalDocs"] stringValue];
-                NSString* endDoc = [[crossQueryResult attributeForName:@"endDoc"] stringValue];
-                
-                for(DDXMLElement* itemElement in xmlItems)
-                {
-                    DDXMLElement *metaElement = [[itemElement nodesForXPath:@"meta" error:&error] objectAtIndex:0];
-                    NSArray* typeNodes = [metaElement nodesForXPath:@"type" error:&error];
-                    if (typeNodes.count) {
-                        DDXMLElement *typeElement = typeNodes[0];
-                        
-                        if ([typeElement.stringValue.lowercaseString isEqualToString:@"image"] || [typeElement.stringValue.lowercaseString isEqualToString:@"image collection"]) {
-                            NSArray* elementArray = [metaElement nodesForXPath:@"reference-image" error:&error];
-                            NSString* imagePath = @"";
-                            if (elementArray.count) {
-                                DDXMLElement *referenceImage = elementArray[0];
-                                NSString* srcString = [[referenceImage attributeForName:@"src"] stringValue];
-                                imagePath = [NSString stringWithFormat:@"http://content.cdlib.org%@", srcString];
-                                
-                                NSURL* imageUrl = [NSURL URLWithString:imagePath];
-                                NSString* titleString = @"";
-                                NSArray* titleNodes = [metaElement nodesForXPath:@"title" error:&error];
-                                if (titleNodes.count) {
-                                    titleString = [titleNodes[0] stringValue];
+            
+            NSArray *facetGroups = [kissXMLOperation.responseXMLDocument nodesForXPath:@"crossQueryResult/facet/group" error:&error];
+            for (DDXMLElement* groupElement in facetGroups) {
+                NSString* groupName = [[groupElement attributeForName:@"value"] stringValue];
+                if ([groupName isEqualToString:@"image"]) {
+                    NSString* totalDocs = [[groupElement attributeForName:@"totalDocs"] stringValue];
+                    NSString* endDoc = [[groupElement attributeForName:@"endDoc"] stringValue];
+                    
+//                    NSLog(@"TOTAL: %@   END: %@", totalDocs, endDoc);
+
+                    NSArray* docHits = [groupElement nodesForXPath:@"docHit" error:&error];
+                    for (DDXMLElement* itemElement in docHits) {
+                        DDXMLElement *metaElement = [[itemElement nodesForXPath:@"meta" error:&error] objectAtIndex:0];
+                        NSArray* typeNodes = [metaElement nodesForXPath:@"type" error:&error];
+                        if (typeNodes.count) {
+                            DDXMLElement *typeElement = typeNodes[0];
+                            
+                            if ([typeElement.stringValue.lowercaseString isEqualToString:@"image"] || [typeElement.stringValue.lowercaseString isEqualToString:@"image collection"]) {
+                                NSArray* elementArray = [metaElement nodesForXPath:@"reference-image" error:&error];
+                                NSString* imagePath = @"";
+                                if (elementArray.count) {
+                                    
+                                    DDXMLElement *referenceImage = elementArray[0];
+                                    NSString* srcString = [[referenceImage attributeForName:@"src"] stringValue];
+                                    imagePath = [NSString stringWithFormat:@"http://content.cdlib.org%@", srcString];
+                                    
+                                    NSURL* imageUrl = [NSURL URLWithString:imagePath];
+                                    NSString* titleString = @"";
+                                    NSArray* titleNodes = [metaElement nodesForXPath:@"title" error:&error];
+                                    if (titleNodes.count) {
+                                        titleString = [titleNodes[0] stringValue];
+                                    }
+                                    
+                                    NSDictionary* opImageDict = @{@"imageUrl": imageUrl, @"title" : titleString};
+                                    OPImageItem* item = [[OPImageItem alloc] initWithDictionary:opImageDict];
+                                    [retArray addObject:item];
                                 }
                                 
-                                NSDictionary* opImageDict = @{@"imageUrl": imageUrl, @"title" : titleString};
-                                OPImageItem* item = [[OPImageItem alloc] initWithDictionary:opImageDict];
-                                [retArray addObject:item];
                             }
-                            
-                        }
+                        }                        
+                    }
+                    if (endDoc.integerValue < totalDocs.integerValue) {
+                        returnCanLoadMore = YES;
                     }
                 }
                 
-                if (endDoc.integerValue < totalDocs.integerValue) {
-                    returnCanLoadMore = YES;
-                }
             }
             
             if (completion) {
