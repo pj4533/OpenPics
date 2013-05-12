6df62eea220cf699101f25b8713305f79a82c775
PJ Gray <pj@evertrue.com>
2013-05-12 18:02:52 -0400

    Added National Library of Australia (Trove)

---
+++ OpenPics/Classes/Providers/Trove/AFTroveClient.m
@@ -0,0 +1,78 @@
+// AFTroveClient.m
+// 
+// Copyright (c) 2013 Say Goodnight Software
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#import "AFTroveClient.h"
+#import "AFJSONRequestOperation.h"
+#import "OPProviderTokens.h"
+
+static NSString * const kAFTroveBaseURLString = @"http://api.trove.nla.gov.au/";
+
+@implementation AFTroveClient
+
++ (AFTroveClient *)sharedClient {
+    static AFTroveClient *_sharedClient = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        
+        _sharedClient = [[AFTroveClient alloc] initWithBaseURL:[NSURL URLWithString:kAFTroveBaseURLString]];
+        
+    });
+    
+    return _sharedClient;
+}
+
+- (id)initWithBaseURL:(NSURL *)url {
+    self = [super initWithBaseURL:url];
+    if (!self) {
+        return nil;
+    }
+    
+    self.parameterEncoding = AFJSONParameterEncoding;
+    
+    [self registerHTTPOperationClass:[AFJSONRequestOperation class]];
+    
+	[self setDefaultHeader:@"Accept" value:@"application/json"];
+    
+    return self;
+}
+
+- (void) getPath:(NSString *)path parameters:(NSDictionary *)parameters success:(void (^)(AFHTTPRequestOperation *, id))success failure:(void (^)(AFHTTPRequestOperation *, NSError *))failure {
+    
+#ifndef kOPPROVIDERTOKEN_TROVE
+    
+#warning *** WARNING: Make sure you have added your Trove token to OPProviderTokens.h!
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"No Token!"
+                                                    message:@"No Trove Token found. Add it to OPProviderTokens.h or use another image source."
+                                                   delegate:nil
+                                          cancelButtonTitle:@"OK"
+                                          otherButtonTitles:nil];
+    [alert show];
+#else
+    NSMutableDictionary* mutableParams = [parameters mutableCopy];
+    mutableParams[@"key"] = kOPPROVIDERTOKEN_TROVE;
+    
+    [super getPath:path parameters:mutableParams success:success failure:failure];
+    
+#endif
+    
+}
+@end
---
+++ OpenPics/Classes/Providers/Trove/OPTroveProvider.m
@@ -0,0 +1,175 @@
+// OPTroveProvider.m
+// 
+// Copyright (c) 2013 Say Goodnight Software
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#import "OPTroveProvider.h"
+#import "OPImageItem.h"
+#import "AFTroveClient.h"
+#import "AFJSONRequestOperation.h"
+
+
+NSString * const OPProviderTypeTrove = @"com.saygoodnight.trove";
+
+@implementation OPTroveProvider
+
+- (id) initWithProviderType:(NSString*) providerType {
+    self = [super initWithProviderType:providerType];
+    if (self) {
+        self.providerName = @"National Library of Australia - Trove";
+    }
+    return self;
+}
+
+- (void) getItemsWithQuery:(NSString*) queryString
+            withPageNumber:(NSNumber*) pageNumber
+                completion:(void (^)(NSArray* items, BOOL canLoadMore))completion {
+    
+    NSDictionary* parameters = @{
+                                 @"q":queryString,
+                                 @"reclevel": @"full",
+                                 @"zone": @"picture",
+                                 @"encoding": @"json",
+                                 @"include": @"holdings,links,subscribinglibs,workversions",
+                                 @"s":[NSString stringWithFormat:@"%d", (pageNumber.integerValue - 1) * 20]
+                                 };
+    
+    
+    NSLog(@"(Trove GET) %@", parameters);
+    
+    [[AFTroveClient sharedClient] getPath:@"result" parameters:parameters success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        
+        NSDictionary* responseDict = responseObject[@"response"];
+        NSArray* zoneArray = responseDict[@"zone"];
+        NSDictionary* pictureZone = zoneArray[0];
+        NSDictionary* recordsDict = pictureZone[@"records"];
+        NSArray* resultArray = recordsDict[@"work"];
+        
+        NSMutableArray* retArray = [NSMutableArray array];
+        if (resultArray) {
+            for (NSDictionary* itemDict in resultArray) {
+                NSMutableDictionary* providerSpecific = [NSMutableDictionary dictionary];
+                
+                NSArray* holdingArray = itemDict[@"holding"];
+                if (holdingArray.count) {
+                    NSDictionary* holdingDict = holdingArray[0];
+                    NSString* holdingName = holdingDict[@"name"];
+                    if (holdingName) {
+                        providerSpecific[@"troveHoldingName"] = holdingName;
+                    }
+                    NSDictionary* holdingUrl = holdingDict[@"url"];
+                    if (holdingUrl) {
+                        if (holdingUrl[@"value"]) {
+                            providerSpecific[@"troveHoldingValue"] = holdingUrl[@"value"];
+                        }
+                    }
+                }
+
+                NSArray* identifierArray = itemDict[@"identifier"];
+                if (identifierArray) {
+                    
+                    NSURL* imageUrl = [NSURL URLWithString:@""];
+                    for (NSDictionary* identifierDict in identifierArray) {
+                        
+                        if ([identifierDict[@"linktype"] isEqualToString:@"thumbnail"]) {
+                            imageUrl = [NSURL URLWithString:identifierDict[@"value"]];
+                        }
+                    }
+
+                    NSString* titleString = @"";
+                    if (itemDict[@"title"]) {
+                        titleString = itemDict[@"title"];
+                    }
+                    if (itemDict[@"snippet"]) {
+                        titleString = itemDict[@"snippet"];
+                    }
+                    NSDictionary* opImageDict = @{
+                                                  @"imageUrl": imageUrl,
+                                                  @"title" : titleString,
+                                                  @"providerSpecific" : providerSpecific
+                                                  };
+                    OPImageItem* item = [[OPImageItem alloc] initWithDictionary:opImageDict];
+                    [retArray addObject:item];
+                }
+            }
+        }
+        
+        BOOL returnCanLoadMore = NO;
+        NSInteger startItem = [recordsDict[@"s"] integerValue];
+        NSInteger lastItem = startItem + 20;
+        NSInteger totalItems = [recordsDict[@"total"] integerValue];
+        if (lastItem < totalItems) {
+            returnCanLoadMore = YES;
+        }
+        
+        NSLog(@"LAST: %d   TOTAL: %d", lastItem, totalItems);
+        if (completion) {
+            completion(retArray, returnCanLoadMore);
+        }
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSLog(@"ERROR: %@\n%@\n%@", error.localizedDescription,error.localizedFailureReason,error.localizedRecoverySuggestion);
+    }];
+}
+
+- (void) fullUpRezItem:(OPImageItem *) item withCompletion:(void (^)(NSURL *uprezImageUrl, OPImageItem* item))completion {
+    
+    NSDictionary* providerSpecific = item.providerSpecific;
+    NSString* urlString = item.imageUrl.absoluteString;
+    
+    NSString* providerName = providerSpecific[@"troveHoldingName"];
+    
+    if ([providerName isEqualToString:@"University of Washington Libraries Digital Collections"]) {
+        NSLog(@"KNOWN: %@", providerName);
+        urlString = [urlString stringByReplacingOccurrencesOfString:@"thumbnail.exe" withString:@"getimage.exe"];
+    } else if ([providerName isEqualToString:@"University of Michigan Library Repository"]) {
+        NSLog(@"KNOWN: %@", providerName);
+        if (providerSpecific[@"troveHoldingValue"]) {
+            NSURL* url = [NSURL URLWithString:providerSpecific[@"troveHoldingValue"]];
+            NSURLRequest *subrequest = [NSURLRequest requestWithURL:url];
+            AFHTTPRequestOperation* httpOperation = [[AFHTTPRequestOperation alloc] initWithRequest:subrequest];
+            [httpOperation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+                NSString* webpageHTML = [[NSString alloc] initWithData:responseObject encoding:NSUTF8StringEncoding];
+                NSArray* components = [webpageHTML componentsSeparatedByString:@";attachment=1\">Largest size"];
+                if (components && components.count) {
+                    NSArray* quotesComponents = [components[0] componentsSeparatedByString:@"\""];
+                    if (quotesComponents && quotesComponents.count) {
+                        if (completion) {
+                            completion([NSURL URLWithString:quotesComponents.lastObject],item);
+                        }
+                    }
+                }
+            } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+                NSLog(@"%@", error);
+            }];
+            [httpOperation start];
+        }
+    } else {
+        NSLog(@"UNKNOWN: %@", providerName);
+    }
+    
+    if (![urlString isEqualToString:item.imageUrl.absoluteString]) {
+        if (completion) {
+            completion([NSURL URLWithString:urlString],item);
+        }
+    }
+    
+}
+
+@end
