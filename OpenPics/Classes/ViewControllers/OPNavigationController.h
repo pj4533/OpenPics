f344db73dc6a22a4a5c2a07b65b4de90e2db91f0
PJ Gray <pj@evertrue.com>
2014-06-16 20:44:32 -0400

    Fixes #109     Orientation fixes

---
+++ OpenPics/Classes/ViewControllers/OPNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  OPNavigationController.h
+//  OpenPics
+//
+//  Created by PJ Gray on 6/16/14.
+//  Copyright (c) 2014 Say Goodnight Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OPNavigationController : UINavigationController
+
+@end
