f346506c177a3a51c5cce4abfc12ce5bb6569bd6
PJ Gray <pj@evertrue.com>
2016-01-02 15:48:43 -0500

    derp

---
+++ OpenPics/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  OpenPics
+//
+//  Created by PJ Gray on 1/2/16.
+//  Copyright © 2016 Say Goodnight Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
